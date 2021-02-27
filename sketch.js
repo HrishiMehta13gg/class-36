a214e7de700bdba389da940d9968c299063f5cac
HrishiMehta13gg <74819313+HrishiMehta13gg@users.noreply.github.com>
2021-02-27 17:42:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database
+var game,player,form;
+var gameState=0;
+var playerCount;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
